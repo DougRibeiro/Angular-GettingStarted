666156ee442a7a94742171522fa0a6a9cf4278d7
Ribeiro, Douglas <Douglas_Ribeiro@Dell.com>
2019-03-23 16:05:20 -0300

    module 10 code

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit {
+
+  constructor(private productService: ProductService) {
+  }
+
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  filteredProducts: IProduct[];
+  private products: IProduct[];
+  errorMessage: string;
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) != -1);
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('in OnInit!')
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = this.products;
+    }, error => this.errorMessage = <any>error);
+    
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+}
